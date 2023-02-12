bbbedb9adae0908ce8ff0e2923f0653b87e0e5dc
nazinamari <93985053+nazinamari@users.noreply.github.com>
2023-02-12 16:58:06 +0000

    update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { theme } from './components/utils/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
