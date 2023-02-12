b9bbdcd5a11aacc36e7ae6e30f908b7e1c808ad0
nazinamari <93985053+nazinamari@users.noreply.github.com>
2023-02-12 16:31:09 +0000

    comit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './components/utils/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
